b69afe989e753535606f48fef58c85c1a561229d
mithun1st <mithun.2121@yahoo.com>
2021-04-06 04:23:26 -0700

    text view

--- TextView/app/src/main/java/com/example/textview/MainActivity.java
+++ TextView/app/src/main/java/com/example/textview/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.textview;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView var1,var2;
+    private TextView var3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        var1 = findViewById(R.id.tv1);
+        var1.setText(R.string.fName);
+        var1.setTextSize(40);
+
+        var2 = findViewById(R.id.tv2);
+        var2.setText("mh Mithun");
+
+
+
+
     }
 }
