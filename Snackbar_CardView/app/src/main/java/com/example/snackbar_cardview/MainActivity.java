a04e4ba6ba7df4dbfdaf205e0e21b15af2a7f04f
mithun1st <mithun.2121@yahoo.com>
2021-07-07 12:42:18 +0600

    CarView and Snackbar

---
+++ Snackbar_CardView/app/src/main/java/com/example/snackbar_cardview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.snackbar_cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private CardView cv;
+    boolean con=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cv=findViewById(R.id.cv);
+        tv=findViewById(R.id.tv);
+
+
+        cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                con=!con;
+
+                if(con){
+                    tv.setVisibility(View.VISIBLE);
+                }
+                else {
+                    tv.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+}
